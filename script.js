c1e8e8589844985f20990ef41957ae0f57999d4b
José Ferreira <ze_francisco_97@hotmail.com>
2022-09-26 20:32:26 +0100

    Add basic functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+const grid = document.querySelector('#grid');
+
+let cells;
+
+function drawGrid(max){
+    for (let i = 0; i < max; i++) {
+
+        let row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < max; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            if (i == 0) cell.classList.add('top-row');
+            if (i == max - 1) cell.classList.add('bot-row');
+            if (j == 0) cell.classList.add('first-col');
+            if (j == max - 1) cell.classList.add('last-col');
+
+            row.append(cell);
+        }
+
+        grid.append(row);
+    }
+
+    cells = document.querySelectorAll('.cell');
+
+}
+
+drawGrid(10);
+
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover', () => {
+        cell.setAttribute('style', 'background-color: black; border-color: black');
+    });
+});
+
+const sizeSelector = document.querySelector('#size-selector');
+
+sizeSelector.addEventListener('click', () => {
+    let max = prompt('Select a grid size');
+    while (grid.hasChildNodes()) grid.removeChild(grid.lastChild);
+    drawGrid(max);
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.setAttribute('style', 'background-color: black; border-color: black');
+        });
+    });
+
+});
